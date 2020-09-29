c6a456ded16a998f077d929856024afc2c7d6e9d
WhitestRabbit <stefanosmixalas@gmail.com>
2020-09-29 19:04:09 +0300

    replaced dummy text with real text, tweaked some css and js

--- js/item.js
+++ js/item.js
@@ -1,4 +1,7 @@
 const params = new URLSearchParams(window.location.search);
-const i = params.get("index");
-document.querySelector('.section__title--intro').firstElementChild.innerHTML += ` ${i}`;
+const i = params.get("index") <= 10 ? params.get("index") : 1;
+const dumpster = document.querySelectorAll('.number-dump');
+dumpster.forEach(element => {
+    element.firstElementChild.innerHTML += ` ${i}`;
+})
 document.querySelector(".intro__img").setAttribute("src", `img/portfolio-${i}.jpg`);
